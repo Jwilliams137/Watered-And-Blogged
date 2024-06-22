4f16c65926f113e39f58065621087c6a2f2294f8
Julie Williams <jwilliams137.036@gmail.com>
2024-06-22 16:23:15 -0400

    another attempt to fix social share image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,14 +21,14 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Open+Sans:wght@400;600&family=Pacifico&display=swap"
         />
         <style>{inter.className}</style>
-        <meta property="og:image" content="<generated>" />
-        <meta property="og:image:type" content="<generated>" />
-        <meta property="og:image:width" content="<generated>" />
-        <meta property="og:image:height" content="<generated>" />
-        <meta name="twitter:image" content="<generated>" />
-        <meta name="twitter:image:type" content="<generated>" />
-        <meta name="twitter:image:width" content="<generated>" />
-        <meta name="twitter:image:height" content="<generated>" />
+        <meta property="og:image" content="/opengraph-image.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1194" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:image" content="/twitter-image.jpg" />
+        <meta name="twitter:image:type" content="image/jpeg" />
+        <meta name="twitter:image:width" content="1194" />
+        <meta name="twitter:image:height" content="630" />
       </head>
       <body className={styles.wrapper}>
         <Nav />
