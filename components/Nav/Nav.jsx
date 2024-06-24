a648dea5872a4349d4d2103543649df16d7a4f68
Julie Williams <jwilliams137.036@gmail.com>
2024-06-23 21:34:10 -0400

    nav

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 'use client';
-import styles from './Nav.module.css';
+import styles from './Nav.module.css'
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { logout } from '../../utils/auth';
